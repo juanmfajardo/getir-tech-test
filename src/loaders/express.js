53f430717ae1ec1dcf938e87f382b86101b31a94
Juan M. Fajardo <juanmfajardo@me.com>
2022-02-13 23:48:21 +0100

    feat(expressInit): improve error handling

--- src/loaders/express.js
+++ src/loaders/express.js
@@ -2,10 +2,19 @@ const cors = require('cors');
 
 const express = require('express');
 const setRoutes = require('../api');
+const responses = require('../api/responses');
 
 const expressInit = (app) => {
     app.use(cors());
-    app.use(express.json());
+    app.use((req, res, next) => {
+        express.json()(req, res, (err) => {
+            if (err) {
+                return responses.badRequest(res, { error: err.message });
+            }
+
+            return next();
+        });
+    });
 
     setRoutes(app);
 };
